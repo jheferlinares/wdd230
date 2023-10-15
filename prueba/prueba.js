04a282e1872e2d050e3acedf6c879b298f846b46
jheferlinares <jhefer98@hotmail.com>
2023-10-14 21:30:06 -0400

    dark mode task made

---
+++ prueba/prueba.js
@@ -0,0 +1,9 @@
+const toggleSwitch = document.getElementById("myToggle");
+
+toggleSwitch.addEventListener("change", function () {
+    if (this.checked) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+});

d10d09966ad58428ce6074c1bfa757033bd07263
jheferlinares <jhefer98@hotmail.com>
2023-11-12 21:40:04 -0400

    visit.js fixed

--- scripts/visits.js
+++ scripts/visits.js
@@ -2,7 +2,7 @@ const visitsDisplay = document.querySelector(".visits");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 1;
 
-if (numVisits !== 0) {
+if (numVisits !== 1) {
 	visitsDisplay.textContent = numVisits;
 } else {
 	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;

15f4a4737e6f7a90a355fa2d3257f0414e069c46
jheferlinares <jhefer98@hotmail.com>
2023-12-13 00:34:15 -0400

    test project

--- project/scripts/responsive.js
+++ project/scripts/responsive.js
@@ -5,18 +5,3 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('open');
 	hambutton.classList.toggle('open');
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-	var imagen = document.getElementById("banner");
-  
-	window.addEventListener("scroll", function() {
-	  var scrollPos = window.scrollY;
-	  var opacityValue = 1 - scrollPos / 500; // Ajusta el divisor para cambiar la velocidad del efecto
-  
-	  // Asegura que la opacidad esté dentro del rango [0, 1]
-	  opacityValue = Math.min(1, Math.max(0, opacityValue));
-  
-	  // Aplica la opacidad al elemento de la imagen
-	  imagen.style.opacity = opacityValue;
-	});
-  });

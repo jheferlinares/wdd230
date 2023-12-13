6b2323a98e34ede43eee1587a252bb10887d0275
jheferlinares <jhefer98@hotmail.com>
2023-12-13 00:21:57 -0400

    test project

--- project/scripts/prices.js
+++ project/scripts/prices.js
@@ -1,6 +1,6 @@
 const baseURL = "https://jheferlinares.github.io/wdd230/";
 const linksURL = "https://jheferlinares.github.io/wdd230/project/data/prices.json";
-const cards = document.querySelector('.grid');
+const cards = document.querySelector('#prices');
 
 async function getCompaniesData() {
   const response = await fetch(linksURL);
@@ -13,6 +13,7 @@ getCompaniesData();
 const displayCompanies = (rentals) => {
     rentals.forEach((rental) => {
       let card = document.createElement('section');
+      card.setAttribute('id','ecard')
       let name = document.createElement('h3');
       let type = document.createElement("h4");
       let MaxPersons = document.createElement("p");
@@ -20,7 +21,6 @@ const displayCompanies = (rentals) => {
       let FullDay = document.createElement("p");
       let portrait = document.createElement('img');
 
-  
       name.textContent = rental.name;
       type.textContent = `Type: ${rental.type}`;
       MaxPersons.textContent = `Max. Persons: ${rental.MaxPersons}`;
@@ -29,16 +29,17 @@ const displayCompanies = (rentals) => {
       portrait.setAttribute('src', rental.image);
       portrait.setAttribute('alt', `Portrait of ${rental.name}`);
       portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '400');
-      portrait.setAttribute('height', '380');
+      portrait.setAttribute('width', '200');
+      portrait.setAttribute('height', '180');
+      portrait.setAttribute('id', 'e-image');
       portrait.setAttribute('background-color', 'white');
   
-      card.appendChild(portrait);
-      card.appendChild(FullDay);
-      card.appendChild(HalfDay);
-      card.appendChild(MaxPersons);
-      card.appendChild(type);
       card.appendChild(name);
+      card.appendChild(type);
+      card.appendChild(MaxPersons);
+      card.appendChild(HalfDay);
+      card.appendChild(FullDay);
+      card.appendChild(portrait);
       cards.appendChild(card);
     });
   };
